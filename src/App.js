c4b19536ee01338a13ddca5008b7c116d916714a
kaduk yevheniy <sartoriamodo@gmail.com>
2020-06-07 16:44:05 +0200

    install router

--- src/App.js
+++ src/App.js
@@ -10,48 +10,23 @@ class App extends React.Component {
   render() {
     return (
     <div className="App">
-     <Header />
+        <Header />
 
-    <div className="container">
-        <main>
-            <h3> Курс валют на 30 ноября 2019</h3>
-            <Rate/>
-            <h3> Калькулятор обмена</h3>
-            <div className="block">
-                <div>Я хочу</div>
-                <div><label><input type="radio" name="radio" defaultValue="0"/> купить</label></div>
-                <div><label><input type="radio" name="radio" defaultValue="1"/> продать</label></div>
-                <div>
-                    <input type="number" defaultValue="150"/>
-                    <select name="" id="">
-                        <option defaultValue="USD">USD</option>
-                        <option defaultValue="RUB">RUB</option>
-                        <option defaultValue="EUR">EUR</option>
-                    </select>
-                </div>
-                <div>
-                    <h4>Результат</h4>
-                    <ul className="calc-res">
-                        <li>EUR 150</li>
-                        <li>EUR 150</li>
-                        <li>EUR 150</li>
-                        <li>EUR 150</li>
-                    </ul>
+        <div className="container">
+            <main>
+    
+                <Rate/>
+            
+            </main>
+            <div className="container" id="cookie_info">
+                <div className="site-content">
+                    <div className="well">На нашем сайте мы используем cookie для сбора информации технического характера.<br/>В
+                        частности, для персонифицированной работы сайта мы обрабатываем IP-адрес региона вашего
+                        местоположения.&nbsp;<button className="btn btn-primary btn-sm">OK</button>
+                    </div>
                 </div>
             </div>
-        </main>
-    </div>
-
-    <div className="container" id="cookie_info">
-        <div className="site-content">
-            <div className="well">На нашем сайте мы используем cookie для сбора информации технического характера.<br/>В
-                частности, для персонифицированной работы сайта мы обрабатываем IP-адрес региона вашего
-                местоположения.&nbsp;<button className="btn btn-primary btn-sm">OK</button></div>
         </div>
-    </div>
-
-
-   
     </div>
   
     );
--- src/Rate/Rate.js
+++ src/Rate/Rate.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import './Rate.css';
-
+import Calc from '../components/Calc/Calc';
 
 
 class Rate extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date : '',
+      currencyRate : {}
+    }
+    this.getRate()
+    this.currency = ['USD', 'RUB', 'CAD', 'PHP'];
+  }
+
+  getRate = () => {
+    fetch('https://api.exchangeratesapi.io/latest')
+    .then(data => {
+      return data.json()
+    })
+    .then( data => {     
+      this.setState({ date: data.date})
+     let result = {};
+      for(let i=0;i<this.currency.length;i++){
+        result[this.currency[i]] = data.rates[this.currency[i]]
+      //  console.log(data.rates[this.currency[i]])
+      }
+      this.setState({currencyRate : result}) 
+    })
+  }
   render() {
     return (
     <div>
-      <div className="flex-container">
-        <div className="block flex-item">
-            <div className="currency-name">USD</div>
-            <div className="currency-in">1500 Kr</div>
-            <div className="currency-out">1200 Kr</div>
+       <h3> Курс валют на {this.state.date}</h3>
+        <div className="flex-container">
+          {Object.keys(this.state.currencyRate).map((keyName, i) =>
+            (
+              <div className="block flex-item" key={i}>
+                <div className="currency-name">{keyName}</div>
+                <div className="currency-in">{this.state.currencyRate[keyName].toFixed(2)}</div>
+                  <p>* Можно купить за 1 ЕUR</p>
+              </div>
+            )
+          )}
         </div>
-        <div className="block flex-item">
-            <div className="currency-name">USD</div>
-            <div className="currency-in">1500 Kr</div>
-            <div className="currency-out">1200 Kr</div>
-        </div>
-        <div className="block flex-item">
-            <div className="currency-name">USD</div>
-            <div className="currency-in">1500 Kr</div>
-            <div className="currency-out">1200 Kr</div>
-        </div>
-      </div>
+        <Calc rate={this.state.currencyRate}/>
     </div>
     );
   }
